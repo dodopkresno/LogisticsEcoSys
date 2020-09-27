432aa9c5e9896bd4e448d9dbf60890cffa3dc5d4
dk_dodopkresno <dodo.kresno@outlook.co.id>
2020-09-28 00:58:12 +0700

    Setup for Domain.Core

---
+++ Domain/Domain.Core/Commands/Command.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        //hanya inherit class ini yang bisa melakukan set
+        public DateTime Timestamp { get; protected set; }
+        public string InitialName { get; protected set; }
+        
+        protected Command(string initialName)
+        {
+            Timestamp = DateTime.Now;
+            InitialName = initialName;
+        }
+    }
+}
---
+++ Domain/Domain.Core/Events/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+        public string InitialName { get; protected set; }
+
+        protected Event(string initialName)
+        {
+            Timestamp = DateTime.Now;
+            InitialName = initialName;
+        }
+    }
+}
