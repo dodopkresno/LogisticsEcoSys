7c216d1fec8d7fa350c911e7e6d2e26f4f853931
dk_dodopkresno <dodo.kresno@outlook.co.id>
2020-10-05 05:05:28 +0700

    Update for RabbitMQ Bus

--- Infra.Bus/MessageBroker.Infra.Bus/RabbitMQBus.cs
+++ Infra.Bus/MessageBroker.Infra.Bus/RabbitMQBus.cs
@@ -111,9 +111,22 @@ namespace MessageBroker.Infra.Bus
             }
         }
 
-        private Task ProcessEvent(string eventName, string message)
+        private async Task ProcessEvent(string eventName, string message)
         {
-            throw new NotImplementedException();
+            if (_handlers.ContainsKey(eventName))
+            {
+                var subscriptions = _handlers[eventName];
+                foreach (var subscription in subscriptions)
+                {
+                    var handler = Activator.CreateInstance(subscription);
+                    if (handler == null) continue;
+                    var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
+                    var @event = JsonConvert.DeserializeObject(message, eventType);
+                    var conreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
+                    await(Task)conreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
+                }
+               
+            }
         }
     }
 }
