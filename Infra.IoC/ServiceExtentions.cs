697fac554aad87cd10efcef22333534afca8689b
dodopkresno <ddgraff2@gmail.com>
2020-10-06 13:31:32 +0700

    Adding Logger Manager

---
+++ Infra.IoC/ServiceExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LoggerService;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Infra.IoC
+{
+    public static class ServiceExtentions
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ILoggerManager, LoggerManager>();
+        }
+    }
+}
