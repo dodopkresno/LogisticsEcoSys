f9aaa7e6c9414d78b884d0b4df117b63d511364c
dk_dodopkresno <dodo.kresno@outlook.co.id>
2020-10-08 13:18:06 +0700

    Adding Measure Type

---
+++ Inventory.Domain/Enums/MeasureType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Inventory.Domain.Enums
+{
+    public enum MeasureType
+    {
+        [Description("Default Units")]
+        Unit,
+        [Description("Default Weight")]
+        Weight,
+        [Description("Default Working Time")]
+        WorkingTime,
+        [Description("Default Length")]
+        Length,
+        [Description("Default Volume")]
+        Volume
+    }
+}
